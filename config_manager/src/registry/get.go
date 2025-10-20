8771f30b3a8b618e4f4ff90226b598c33f245ec8
NM <nmanojlovic98@gmail.com>
2025-10-20 19:42:00 +0200

    Some changes to the config schema and added some registry related funcs

---
+++ config_manager/src/registry/get.go
@@ -0,0 +1,109 @@
+package registry
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"slices"
+	"strings"
+	"time"
+
+	getter "github.com/hashicorp/go-getter"
+)
+
+// RegistryGitDownload downloads the IBC registry from the GitHub repository
+//
+// Params:
+//	- dst: the directory to download the registry to
+//
+// Returns:
+//	- error: if the registry cannot be downloaded
+//
+// Usage:
+//	- Used to download the IBC registry from the GitHub repository
+func RegistryGitDownload(dst string) error {
+	// format for using go getter
+	url := "https://github.com/cosmos/chain-registry.git//_IBC"
+	deadline := time.Now().Add(30 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+
+	opts := getter.Client{
+		Ctx: ctx,
+		Src: url,
+		Dst: dst,
+		Mode: getter.ClientModeDir,
+		Getters: map[string]getter.Getter{
+			"git": &getter.GitGetter{},
+		},
+	}
+	log.Printf("Downloading registry from %s to %s", url, dst)
+	err := opts.Get()
+	if err != nil {
+		return fmt.Errorf("failed to download registry: %w", err)
+	}
+	return nil
+}
+
+// ProcessIbcRegistry processes the IBC registry and returns the data
+//
+// Params:
+//	- dst: the directory to read the registry from, this is the directory where the registry is downloaded to
+//	- keywords: the keywords to search for, this is the keywords pulled out from chain configurations by
+//  looking at the registry string
+//
+// Returns:
+//	- []ChainIbcData: the IBC data
+//	- error: if the registry cannot be read or processed
+//
+// Usage:
+//	- Used to process the IBC registry and return the data
+func ProcessIbcRegistry(dst string, keywords []string) ([]ChainIbcData, error) {
+	ibcData := []ChainIbcData{}
+	// read the registry from the file
+	files, err := os.ReadDir(dst)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read registry: %w", err)
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		name := file.Name()
+		// check only json files
+		// and check if they contain any kind of keyword
+		// split them by - in the name and verify if they are in the keyword
+		if strings.HasSuffix(name, ".json") {
+			name = strings.TrimSuffix(name, ".json")
+			chainNames := strings.Split(name, "-")
+				if slices.Contains(keywords, chainNames[0]) && slices.Contains(keywords, chainNames[1]) {
+					// open the file and read the contents with untrimmed name
+					filePath := fmt.Sprintf("%s/%s", dst, file.Name())
+					jsonFile, err := os.Open(filePath)
+					if err != nil {
+						return nil, fmt.Errorf("failed to open file: %w", err)
+					}
+					defer func(){
+						if err := jsonFile.Close(); err != nil {
+							log.Fatalf("Failed to close file: %v", err)
+						}
+					}()
+					body, err := io.ReadAll(jsonFile)
+					if err != nil {
+						return nil, fmt.Errorf("failed to read file: %w", err)
+					}
+					var data ChainIbcData
+					err = json.Unmarshal(body, &data)
+					if err != nil {
+						return nil, fmt.Errorf("failed to unmarshal file: %w", err)
+					}
+					ibcData = append(ibcData, data)
+				}		
+		}
+
+	}
+	return ibcData, nil
+}
---
+++ config_manager/src/registry/types.go
@@ -0,0 +1,70 @@
+package registry
+
+// Main json struct object from the chain registry
+type ChainIbcData struct {
+		Schema string `json:"$schema"`
+		Chain1 IbcChainData `json:"chain_1"`
+		Chain2 IbcChainData `json:"chain_2"`
+		Channels []IbcChannelData `json:"channels"`
+}
+
+// IBC chain data
+type IbcChainData struct {
+	ChainName    string `json:"chain_name"`
+	ClientID     string `json:"client_id"`
+	ConnectionID string `json:"connection_id"`
+}
+
+// IBC channel data
+type IbcChannelData struct {
+	Chain1 ChannelChainData `json:"chain_1"`
+	Chain2 ChannelChainData `json:"chain_2"`
+	Ordering string `json:"ordering"`
+	Version  string `json:"version"`
+	Tags     ChannelTags `json:"tags"`
+}
+
+
+// Channel chain data
+type ChannelChainData struct {
+	ChannelID string `json:"channel_id"`
+	PortID    string `json:"port_id"`
+}
+
+// Channel tags
+type ChannelTags struct {
+	Preferred bool   `json:"preferred"`
+	Status    string `json:"status"`
+}
+
+
+/* 
+The internal Spectra IBC Hub registry
+
+It should hold the information about the chains and channels that are supported by the Spectra IBC Hub.
+This should be a part of the final config at least for now.
+*/
+type IbcRegistry struct {
+	Chains []IbcChain `toml:"chains"`
+}
+
+/*
+IbcChain is a struct that holds the information about a chain that is supported by the Spectra IBC Hub.
+*/
+type IbcChain struct {
+	ChainName string `toml:"chain_name"`
+	ChainId string `toml:"chain_id"`
+	Channels []IbcChannel `toml:"channels"`
+}
+
+/*
+IbcChannel is a struct that holds the information about a channel such as channel data and to which
+chain it is connected to.
+*/
+type IbcChannel struct {
+	ChannelId string `toml:"channel_id"`
+	PortId string `toml:"port_id"`
+	ConnectionId string `toml:"connection_id"`
+	ToChainName string `toml:"to_chain_name"`
+	ToChainId string `toml:"to_chain_id"`
+}
