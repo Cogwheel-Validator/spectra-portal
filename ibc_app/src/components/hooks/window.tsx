94baa76cf6325150c1f8dccd8f2aab67cc665c60
NM <nmanojlovic98@gmail.com>
2025-11-27 21:14:44 +0100

    Updated client app dependencies and added some initial home page and other componenets

---
+++ ibc_app/src/components/hooks/window.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export function useWindowSize(): { width: number; height: number } {
+    const [windowSize, setWindowSize] = useState<{ width: number; height: number }>({
+        width: typeof window !== "undefined" ? window.innerWidth : 0,
+        height: typeof window !== "undefined" ? window.innerHeight : 0,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowSize;
+}
