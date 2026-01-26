4c5d93f31ae130e05ae122915365dd9a9f2f7320
NM <nmanojlovic98@gmail.com>
2026-01-26 11:53:23 +0100

    Small fix, the senderUi still sends the true on the singleRoute when sending request to the pathfinder.

--- client_app/src/components/ui/send/senderUi.tsx
+++ client_app/src/components/ui/send/senderUi.tsx
@@ -220,7 +220,7 @@ export default function SendUI({
             tokenToDenom: selectedReceiveToken?.denom ?? "",
             senderAddress,
             receiverAddress,
-            singleRoute: true,
+            singleRoute: false,
             slippageBps: 100,
         };
     }, [
