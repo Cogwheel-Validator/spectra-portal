0986720baf2781d73e92be8b4795d711523cd8fc
NM <nmanojlovic98@gmail.com>
2026-02-14 10:27:21 +0100

    Add dockerfile for the client app

--- client_app/next.config.ts
+++ client_app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: process.env.DOCKER_BUILD === "true" ? "standalone" : undefined,
   reactCompiler: true,
 };
 
